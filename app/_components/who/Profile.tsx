2e5577932ededb7f706eefbb2458975946da24b1
josephclander <josephclander@gmail.com>
2024-04-25 16:39:56 +0100

    Add Profiles

---
+++ app/_components/who/Profile.tsx
@@ -0,0 +1,50 @@
+// file: largebytes/app/_components/who/Profile.tsx
+
+import Image from "next/image";
+import LinkedInLogo from "./LinkedInLogo";
+
+type ProfileProps = {
+  alt: string;
+  src: string;
+  name: string;
+  title: string;
+  linkedin: string;
+  text: string;
+};
+
+const Profile: React.FC<ProfileProps> = ({
+  alt,
+  src,
+  name,
+  title,
+  linkedin,
+  text,
+}) => {
+  return (
+    <div className="flex flex-col gap-8 items-center">
+      <div className="flex justify-center w-9/12 max-w-sm">
+        <Image
+          width={600}
+          height={600}
+          alt={alt}
+          src={src}
+          layout="responsive"
+        />
+      </div>
+      <div className="bg-white flex flex-col px-2 py-5 gap-5">
+        <div className="flex justify-between items-center">
+          <div>
+            <div className="text-2xl font-semibold">{name}</div>
+            <div className="text-2xl font-light">{title}</div>
+          </div>
+          <a target="_blank" href={linkedin}>
+            <LinkedInLogo />
+          </a>
+        </div>
+        <div>{text}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
--- app/_components/who/Who.tsx
+++ app/_components/who/Who.tsx
@@ -1,39 +1,36 @@
 // file: largebytes/app/_components/who/Who.tsx
 
-import Image from "next/image";
 import SectionTitle from "../utils/SectionTitle";
-import LinkedInLogo from "./LinkedInLogo";
+import Profile from "./Profile";
+
+const profiles = [
+  {
+    alt: "Character of Tom",
+    src: "/TomImage_600x600.png",
+    name: "Tom Fyfe",
+    title: "Backend Developer",
+    linkedin: "https://www.linkedin.com/in/tomfyfe/",
+    text: "Experienced building organised, well tested products specialising in Python with Django. Tom let’s loose as a talented musician playing drums around the country when he’s not at his keyboard.",
+  },
+  {
+    alt: "Character of Joe",
+    src: "/JoeImage_600x600.png",
+    name: "Joe Lander",
+    title: "Front End Developer",
+    linkedin: "https://www.linkedin.com/in/josephclander/",
+    text: "React and TypeScript are Joe’s favourite tools to create responsive user-experiences with modern CSS. In a past career he was a Maths teacher so is no stranger to the tougher logical aspects of the job.",
+  },
+];
 
 const Who = () => {
   return (
-    <section className="p-5 bg-secondary-blue">
+    <section className="flex flex-col py-4 px-5 bg-secondary-blue">
       <SectionTitle>Who We Are</SectionTitle>
-      <div className="flex flex-col gap-8 items-center">
-        <div className="flex justify-center w-9/12 max-w-sm">
-          <Image
-            width={600}
-            height={600}
-            alt="Character of Tom"
-            src="/TomImage_600x600.png"
-            layout="responsive"
-          />
-        </div>
-        <div className="bg-white flex flex-col px-2 py-5 gap-5">
-          <div className="flex justify-between items-center">
-            <div>
-              <div className="text-2xl font-semibold">Tom Fyfe</div>
-              <div className="text-2xl font-light">Backend Developer</div>
-            </div>
-            <a target="_blank" href="https://www.linkedin.com/in/tomfyfe/">
-              <LinkedInLogo />
-            </a>
-          </div>
-          <div>
-            Experienced building organised, well tested products specialising in
-            Python with Django. Tom let’s loose as a talented musician playing
-            drums around the country when he’s not at his keyboard.
-          </div>
-        </div>
+      <div className="flex flex-col gap-16 py-8">
+        {profiles &&
+          profiles.map((profile) => (
+            <Profile key={profile.name} {...profile} />
+          ))}
       </div>
     </section>
   );
