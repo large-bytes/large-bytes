a16236b3b99eff6c29fba37c5e3931c41a40e6bf
josephclander <josephclander@gmail.com>
2024-04-25 13:53:20 +0100

    Refactor Header

---
+++ app/_components/landing/Header.tsx
@@ -0,0 +1,14 @@
+// file: largebytes/app/_components/landing/Header.tsx
+
+import LogoSmall from "../utils/LogoSmall";
+
+const Header: React.FC = () => {
+  return (
+    <header className="flex gap-2 items-center">
+      <LogoSmall width={"60px"} height={"60px"} />
+      <h1 className="font-black text-3xl text-primary-blue">LARGE BYTES</h1>
+    </header>
+  );
+};
+
+export default Header;
--- app/_components/landing/Landing.tsx
+++ app/_components/landing/Landing.tsx
@@ -1,16 +1,14 @@
 // file: largebytes/app/_components/Landing.tsx
 
 import Button from "../utils/Button";
+import Header from "./Header";
 import Hero from "./hero/Hero";
-import LogoSmall from "../utils/LogoSmall";
+
 
 const Landing: React.FC = () => {
   return (
     <div className="bg-secondary-green p-2.5 flex flex-col gap-10 pb-10">
-      <header className="flex gap-2 items-center">
-        <LogoSmall width={"60px"} height={"60px"} />
-        <h1 className="font-black text-3xl text-primary-blue">LARGE BYTES</h1>
-      </header>
+      <Header />
       <Hero />
       <Button>Explore Products</Button>
     </div>
