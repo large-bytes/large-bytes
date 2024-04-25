50b088aac7602464f3111c4cddf88155275ec961
josephclander <josephclander@gmail.com>
2024-04-25 13:44:41 +0100

    Update heroImage size

--- app/_components/HeroImage.tsx
+++ app/_components/HeroImage.tsx
@@ -4,8 +4,8 @@ const HeroImage: React.FC = () => {
   return (
     <svg
       className="self-end"
-      width="60%"
-      height="60%"
+      width="75%"
+      height="75%"
       xmlns="http://www.w3.org/2000/svg"
       xmlSpace="preserve"
       style={{
