adbedb3b586e8958e93bef703dea623f252812e7
josephclander <josephclander@gmail.com>
2024-04-26 08:50:46 +0100

    Fix Large Bytes Logo SVG

--- app/_components/utils/LargeBytesLogo.tsx
+++ app/_components/utils/LargeBytesLogo.tsx
@@ -5,256 +5,205 @@ const LargeBytesLogo = () => {
     <svg
       className="w-full h-full"
       xmlns="http://www.w3.org/2000/svg"
-      xmlSpace="preserve"
-      style={{
-        fillRule: "evenodd",
-        clipRule: "evenodd",
-        strokeLinecap: "round",
-        strokeLinejoin: "round",
-        strokeMiterlimit: 1.5,
-      }}
+      fillRule="evenodd"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeMiterlimit="1.5"
+      clipRule="evenodd"
       viewBox="0 0 600 600"
     >
-      <path
-        d="M0 0h600v600H0z"
-        style={{
-          fill: "none",
-        }}
-      />
-      <path
-        d="M0 0h600v600H0z"
-        style={{
-          fill: "#1ba1e4",
-        }}
-      />
-      <path d="M600 0v600H0V0h600Zm-5.5 5.5H5.5v589h589V5.5Z" />
-      <path
-        d="M51 68h434v434H51z"
-        style={{
-          fill: "#1be4c2",
-          stroke: "#000",
-          strokeWidth: "4.58px",
-        }}
-        transform="matrix(1.20123 -.00384 .00384 1.20123 -23.014 -41.322)"
-      />
-      <g opacity={0.6}>
-        <path
-          d="M72.84 199.514h145.123"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(1.1136 0 0 1.1104 -33.794 -9.024)"
-        />
-        <path
-          d="M68.721 199.514H212"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(1.1136 0 0 1.1104 -30.236 49.298)"
-        />
-        <path
-          d="M68.811 199.514H212"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(1.1136 0 0 1.1104 -30.236 107.363)"
-        />
-        <path
-          d="M65.944 199.514H212"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(1.1136 0 0 1.1104 -27.154 165.943)"
-        />
-        <path
-          d="M58.476 199.514H212"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(1.1136 0 0 1.1104 318.78 -9.024)"
-        />
-        <path
-          d="M55.281 199.514h151.733"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(1.1136 0 0 1.1104 322.339 49.298)"
-        />
-        <path
-          d="M52.513 199.514h151.734"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(1.1136 0 0 1.1104 325.42 107.62)"
-        />
-        <path
-          d="M52.513 199.514h151.734"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(1.1136 0 0 1.1104 325.42 165.943)"
-        />
-        <path
-          d="M64.927 199.514H212"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(0 -1.11362 1.1104 0 -12.606 281.228)"
-        />
-        <path
-          d="M58.506 199.514h149.506"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(0 -1.11362 1.1104 0 45.716 277.67)"
-        />
-        <path
-          d="M55.738 199.514H205.02"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(0 -1.11362 1.1104 0 104.038 274.587)"
-        />
-        <path
-          d="M55.738 199.514H205.02"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(0 -1.11362 1.1104 0 162.36 274.587)"
-        />
-        <path
-          d="M68.915 199.514h149.048"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(0 -1.11362 1.1104 0 -12.606 630.21)"
-        />
-        <path
-          d="M64.927 199.514H212"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(0 -1.11362 1.1104 0 45.716 626.651)"
-        />
-        <path
-          d="M64.927 199.514H212"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(0 -1.11362 1.1104 0 104.038 623.57)"
-        />
-        <path
-          d="M64.927 199.514H212"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="matrix(0 -1.11362 1.1104 0 162.36 623.57)"
-        />
-        <path
-          d="M217.98 221.215h157.57v157.57H217.98z"
-          style={{
-            fill: "none",
-            stroke: "#21c5a7",
-            strokeWidth: "4.95px",
-            strokeLinecap: "square",
-          }}
-          transform="translate(-33.111 -33.121) scale(1.1104)"
-        />
+      <g transform="translate(-764)">
+        <g transform="translate(764)">
+          <path fill="none" d="M0 0H600V600H0z"></path>
+          <path fill="#1BA1E4" d="M0 0H600V600H0z"></path>
+          <path d="M600 0v600H0V0h600zm-5.5 5.5H5.5v589h589V5.5z"></path>
+          <path
+            fill="#1BE4C2"
+            stroke="#000"
+            strokeWidth="4.58"
+            d="M51 68H485V502H51z"
+            transform="translate(-33.111 -33.121) scale(1.1104) matrix(1.0818 -.00346 .00346 1.0818 9.093 -7.386)"
+          ></path>
+          <g opacity="0.6" transform="translate(-33.111 -33.121) scale(1.1104)">
+            <path
+              fill="none"
+              stroke="#21C5A7"
+              strokeLinecap="square"
+              strokeWidth="4.95"
+              d="M72.84 199.514h145.123"
+              transform="matrix(1.0029 0 0 1 -.615 21.701)"
+            ></path>
+            <path
+              fill="none"
+              stroke="#21C5A7"
+              strokeLinecap="square"
+              strokeWidth="4.95"
+              d="M68.721 199.514H212"
+              transform="matrix(1.0029 0 0 1 2.59 74.225)"
+            ></path>
+            <path
+              fill="none"
+              stroke="#21C5A7"
+              strokeLinecap="square"
+              strokeWidth="4.95"
+              d="M68.811 199.514H212"
+              transform="matrix(1.0029 0 0 1 2.59 126.517)"
+            ></path>
+            <path
+              fill="none"
+              stroke="#21C5A7"
+              strokeLinecap="square"
+              strokeWidth="4.95"
+              d="M65.944 199.514H212"
+              transform="matrix(1.0029 0 0 1 5.365 179.272)"
+            ></path>
+            <path
+              fill="none"
+              stroke="#21C5A7"
+              strokeLinecap="square"
+              strokeWidth="4.95"
+              d="M58.476 199.514H212"
+              transform="translate(317.52) matrix(1.0029 0 0 1 -.615 21.701)"
+            ></path>
+            <path
+              fill="none"
+              stroke="#21C5A7"
+              strokeLinecap="square"
+              strokeWidth="4.95"
+              d="M55.281 199.514h151.733"
+              transform="translate(317.52) matrix(1.0029 0 0 1 2.59 74.225)"
+            ></path>
+            <path
+              fill="none"
+              stroke="#21C5A7"
+              strokeLinecap="square"
+              strokeWidth="4.95"
+              d="M52.513 199.514h151.734"
+              transform="translate(317.52) matrix(1.0029 0 0 1 5.365 126.748)"
+            ></path>
+            <path
+              fill="none"
+              stroke="#21C5A7"
+              strokeLinecap="square"
+              strokeWidth="4.95"
+              d="M52.513 199.514h151.734"
+              transform="translate(317.52) matrix(1.0029 0 0 1 5.365 179.272)"
+            ></path>
+            <g>
+              <path
+                fill="none"
+                stroke="#21C5A7"
+                strokeLinecap="square"
+                strokeWidth="4.95"
+                d="M64.927 199.514H212"
+                transform="rotate(-90 139.623 142.857) matrix(1.0029 0 0 1 -.615 21.701)"
+              ></path>
+              <path
+                fill="none"
+                stroke="#21C5A7"
+                strokeLinecap="square"
+                strokeWidth="4.95"
+                d="M58.506 199.514h149.506"
+                transform="rotate(-90 139.623 142.857) matrix(1.0029 0 0 1 2.59 74.225)"
+              ></path>
+              <path
+                fill="none"
+                stroke="#21C5A7"
+                strokeLinecap="square"
+                strokeWidth="4.95"
+                d="M55.738 199.514H205.02"
+                transform="rotate(-90 139.623 142.857) matrix(1.0029 0 0 1 5.365 126.748)"
+              ></path>
+              <path
+                fill="none"
+                stroke="#21C5A7"
+                strokeLinecap="square"
+                strokeWidth="4.95"
+                d="M55.738 199.514H205.02"
+                transform="rotate(-90 139.623 142.857) matrix(1.0029 0 0 1 5.365 179.272)"
+              ></path>
+            </g>
+            <g>
+              <path
+                fill="none"
+                stroke="#21C5A7"
+                strokeLinecap="square"
+                strokeWidth="4.95"
+                d="M68.915 199.514h149.048"
+                transform="rotate(-90 296.765 300) matrix(1.0029 0 0 1 -.615 21.701)"
+              ></path>
+              <path
+                fill="none"
+                stroke="#21C5A7"
+                strokeLinecap="square"
+                strokeWidth="4.95"
+                d="M64.927 199.514H212"
+                transform="rotate(-90 296.765 300) matrix(1.0029 0 0 1 2.59 74.225)"
+              ></path>
+              <path
+                fill="none"
+                stroke="#21C5A7"
+                strokeLinecap="square"
+                strokeWidth="4.95"
+                d="M64.927 199.514H212"
+                transform="rotate(-90 296.765 300) matrix(1.0029 0 0 1 5.365 126.748)"
+              ></path>
+              <path
+                fill="none"
+                stroke="#21C5A7"
+                strokeLinecap="square"
+                strokeWidth="4.95"
+                d="M64.927 199.514H212"
+                transform="rotate(-90 296.765 300) matrix(1.0029 0 0 1 5.365 179.272)"
+              ></path>
+            </g>
+            <path
+              fill="none"
+              stroke="#21C5A7"
+              strokeLinecap="square"
+              strokeWidth="4.95"
+              d="M217.98 221.215H375.54999999999995V378.78499999999997H217.98z"
+            ></path>
+          </g>
+          <g
+            fill="#1BA1E4"
+            stroke="#000"
+            strokeLinecap="butt"
+            strokeMiterlimit="2"
+            strokeWidth="0.57"
+            fontFamily="'Arial-Black', 'Arial Black', sans-serif"
+            fontSize="12"
+            fontWeight="900"
+          >
+            <text
+              x="112.966"
+              y="251.325"
+              transform="translate(-1010.45 -2128.03) scale(9.57491)"
+            >
+              L
+              <tspan
+                x="121.558px 131.48px 141.185px 151.769px"
+                y="251.325px 251.325px 251.325px 251.325px"
+              >
+                ARGE
+              </tspan>
+            </text>
+            <text
+              x="112.966"
+              y="264.43"
+              transform="translate(-1010.45 -2128.03) scale(9.57491)"
+            >
+              B
+              <tspan
+                x="122.888px 132.81px 142.064px 151.318px"
+                y="264.43px 264.43px 264.43px 264.43px"
+              >
+                YTES
+              </tspan>
+            </text>
+          </g>
+        </g>
       </g>
-      <text
-        x={112.966}
-        y={251.325}
-        style={{
-          fontFamily: "&quot",
-          fontWeight: 900,
-          fontSize: 12,
-          fill: "#1ba1e4",
-          stroke: "#000",
-          strokeWidth: ".57px",
-          strokeLinecap: "butt",
-          strokeMiterlimit: 2,
-        }}
-        transform="translate(-1010.45 -2128.03) scale(9.57491)"
-      >
-        {"L"}
-        <tspan
-          x="121.558px 131.48px 141.185px 151.769px"
-          y="251.325px 251.325px 251.325px 251.325px"
-        >
-          {"ARGE"}
-        </tspan>
-      </text>
-      <text
-        x={112.966}
-        y={264.43}
-        style={{
-          fontFamily: "&quot",
-          fontWeight: 900,
-          fontSize: 12,
-          fill: "#1ba1e4",
-          stroke: "#000",
-          strokeWidth: ".57px",
-          strokeLinecap: "butt",
-          strokeMiterlimit: 2,
-        }}
-        transform="translate(-1010.45 -2128.03) scale(9.57491)"
-      >
-        {"B"}
-        <tspan
-          x="122.888px 132.81px 142.064px 151.318px"
-          y="264.43px 264.43px 264.43px 264.43px"
-        >
-          {"YTES"}
-        </tspan>
-      </text>
     </svg>
   );
 };
