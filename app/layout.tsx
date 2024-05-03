7b4950c1773b6d4d6f0814d0f7912ed07583cd98
josephclander <josephclander@gmail.com>
2024-05-03 11:26:07 +0100

    Update meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Large Bytes - Creating software solutions for small businesses.",
-  description: "Creating software solutions for small businesses.",
+  description:
+    "At Large Bytes, we specialize in custom software development, IT consultancy, and system integration tailored to empower small businesses with affordable, scalable technology solutions. Discover how we can help your business grow at largebytes.co.uk.",
+  authors: [
+    { name: "Joe Lander", url: "https://www.linkedin.com/in/josephclander/" },
+    { name: "Tom Fyfe", url: "https://www.linkedin.com/in/tomfyfe/" },
+  ],
+  keywords: [
+    "software solutions for small businesses",
+    "custom software development",
+    "IT consultancy services",
+    "system integration solutions",
+    "small business technology solutions",
+    "UK software solutions",
+    "affordable business software",
+    "scalable IT solutions for SMEs",
+  ],
+  creator: "Joe Lander",
 };
 
 export default function RootLayout({
