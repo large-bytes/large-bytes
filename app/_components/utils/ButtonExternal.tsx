dc63555f770515eee0d3fb20a46e9ea169fdcde9
josephclander <josephclander@gmail.com>
2024-05-23 12:43:07 +0100

    Add external target to ButtonExternal

--- app/_components/utils/ButtonExternal.tsx
+++ app/_components/utils/ButtonExternal.tsx
@@ -9,6 +9,7 @@ const ButtonExternal = ({ children, link }: ButtonExternalProps) => {
     <a
       href={link}
       className="bg-primary-blue hover:bg-accent-red active:bg-accent-red duration-300 text-white border-2 border-black px-5 py-3 max-w-lg self-center"
+      target="_blank"
     >
       {children}
     </a>
